3faf79e92aed1dbac37e027c58585b6d6b9e8b8d
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-05-24 23:06:17 +0200

    Fixed: accessLevel in edituser blade
    Renamed accessLevel to access_level

--- resources/views/management/user/edituser.blade.php
+++ resources/views/management/user/edituser.blade.php
@@ -11,7 +11,7 @@
     @endif
     <form method="POST" action="">
         {{ csrf_field() }}
-        @if($user->accessLevel >= Auth::user()->accessLevel)
+        @if($user->access_level >= Auth::user()->access_level)
             <div class="row">
                 <h4 class="col-xs-12 text-danger text-center">Let op: Je hebt niet de rechten om deze gebruiker aan te passen!</h4>
             </div>
@@ -25,7 +25,7 @@
             <div class="col-xs-12 col-md-offset-3 col-md-6">
                 <div class="form-group">
                     <label for="username">Gebruikersnaam:</label>
-                    <input class="form-control" type="text" id="username" name="username" placeholder="username" value="{{ old('username', $user->username) }}" required {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }} />
+                    <input class="form-control" type="text" id="username" name="username" placeholder="username" value="{{ old('username', $user->username) }}" required {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }} />
                 </div>
                 @if ($errors->has('username'))
                     <span class="help-block">
@@ -38,7 +38,7 @@
             <div class="col-xs-12 col-md-offset-3 col-md-6">
                 <div class="form-group">
                     <label for="fullname">Volledige naam:</label>
-                    <input class="form-control" type="text" id="fullname" name="fullname" placeholder="Volledige naam" value="{{ old('fullname', $user->fullname) }}" required {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }} />
+                    <input class="form-control" type="text" id="fullname" name="fullname" placeholder="Volledige naam" value="{{ old('fullname', $user->fullname) }}" required {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }} />
                 </div>
                 @if ($errors->has('fullname'))
                     <span class="help-block">
@@ -59,7 +59,7 @@
             <div class="col-xs-12 col-md-offset-3 col-md-6">
                 <div class="form-group">
                     <label for="email">Email adres:</label>
-                    <input class="form-control" type="email" id="email" name="email" placeholder="email" value="{{ old('email', $user->email) }}" required {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }} />
+                    <input class="form-control" type="email" id="email" name="email" placeholder="email" value="{{ old('email', $user->email) }}" required {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }} />
                 </div>
                 @if ($errors->has('email'))
                     <span class="help-block">
@@ -72,7 +72,7 @@
             <div class="col-xs-12 col-md-offset-3 col-md-6">
                 <div class="form-group">
                     <label for="email">Wachtwoord (Houd leeg om niet aan te passen):</label>
-                    <input class="form-control" type="password" id="password" name="password" placeholder="(Houd deze leeg om niet aan te passen)" {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }} />
+                    <input class="form-control" type="password" id="password" name="password" placeholder="(Houd deze leeg om niet aan te passen)" {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }} />
                 </div>
                 @if ($errors->has('password'))
                     <span class="help-block">
@@ -84,30 +84,30 @@
         <div class="row">
             <div class="col-xs-12 col-md-offset-3 col-md-6">
                 <div class="form-group">
-                    <label for="accesslevel">Access level:</label>
-                    <select class="form-control" id="accesslevel" name="accesslevel" {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }}>
-                        <option value="0" {{ ($user->access_level===\App\Models\AccessLevel::USER)?"selected":"" }}>User</option>
-                        <option value="1" {{ ($user->access_level===\App\Models\AccessLevel::ADMIN)?"selected":"" }} {{ Auth::user()->hasPermission(\App\Models\AccessLevel::WEBMASTER)?"":"disabled" }}>Admin</option>
-                        <option value="2" {{ ($user->access_level===\App\Models\AccessLevel::WEBMASTER)?"selected":"" }} disabled>Webmaster</option>
+                    <label for="access_level">Access level:</label>
+                    <select class="form-control" id="access_level" name="access_level" {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }}>
+                        <option value="0" {{ ($user->access_level===\App\Models\access_level::USER)?"selected":"" }}>User</option>
+                        <option value="1" {{ ($user->access_level===\App\Models\access_level::ADMIN)?"selected":"" }} {{ Auth::user()->hasPermission(\App\Models\access_level::WEBMASTER)?"":"disabled" }}>Admin</option>
+                        <option value="2" {{ ($user->access_level===\App\Models\access_level::WEBMASTER)?"selected":"" }} disabled>Webmaster</option>
                     </select>
                 </div>
-                @if ($errors->has('accesslevel'))
+                @if ($errors->has('access_level'))
                     <span class="help-block">
-                        <strong>{{ $errors->first('accesslevel') }}</strong>
+                        <strong>{{ $errors->first('access_level') }}</strong>
                     </span>
                 @endif
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-md-offset-3 col-md-6">
-                <input type="checkbox" id="verified" name="verified" {{ old('verified', $user->verified)?"checked":"" }}  {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }}/>
+                <input type="checkbox" id="verified" name="verified" {{ old('verified', $user->verified)?"checked":"" }}  {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }}/>
                 <label for="verified">geverifieerd</label>
             </div>
         </div>
         <div class="row">
             <div class="col-xs-12 col-sm-6 col-md-offset-3 col-md-4">
                 <br/>
-                <input class="btn btn-success" type="submit" value="Update" {{ ($user->accessLevel >= Auth::user()->accessLevel)?"disabled":"" }} />
+                <input class="btn btn-success" type="submit" value="Update" {{ ($user->access_level >= Auth::user()->access_level)?"disabled":"" }} />
             </div>
         </div>
     </form>
