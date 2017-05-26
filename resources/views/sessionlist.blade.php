0370f54c7098d02fb09b5eb6b7ee909690ee4824
Thomas 'SubSide' van den Bulk <Thomasvdbulk@gmail.com>
2017-05-26 18:17:11 +0200

    Changed: Only show tables if there exists sessions
    We now only show the sessions if there are actually sessions planned. If
    no sessions are planned, show a simple text and, if logged in, a link to
    how to create a new session.

--- resources/views/sessionlist.blade.php
+++ resources/views/sessionlist.blade.php
@@ -1,9 +1,26 @@
 @extends('base')
 
 @section('content')
-    @include('macros.sessiontable', ['title' => 'Sessies deze week', 'sessions' => $thisWeek])
-    <br /><br />
-    @include('macros.sessiontable', ['title' => 'Sessies volgende week', 'sessions' => $nextWeek])
-    <br /><br />
-    @include('macros.sessiontable', ['title' => 'Toekomstige sessies', 'sessions' => $future])
+    @if(count($thisWeek) > 0)
+        @include('macros.sessiontable', ['title' => 'Sessies deze week', 'sessions' => $thisWeek])
+        <br />
+    @endif
+
+    @if(count($nextWeek) > 0)
+        <br />
+        @include('macros.sessiontable', ['title' => 'Sessies volgende week', 'sessions' => $nextWeek])
+        <br />
+    @endif
+
+    @if(count($future) > 0)
+        <br />
+        @include('macros.sessiontable', ['title' => 'Toekomstige sessies', 'sessions' => $future])
+    @endif
+
+    @if(count($thisWeek) < 1 && count($nextWeek) < 1 && count($future) < 1)
+        Er zijn nog geen sessies gepland!<br /><br/>
+        @if(Auth::check())
+            Wil je zelf een sessie runnen? <a href="{{ route('me.sessions') }}">Klik hier!</a>
+        @endif
+    @endif
 @endsection
